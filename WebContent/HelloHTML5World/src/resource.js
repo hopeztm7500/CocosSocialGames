4466fc94b1dd7f83731ae3100030be1af79b6c57
Victor <hopeztm@163.com>
2014-08-06 06:54:57 +0800

    first commit

---
+++ WebContent/HelloHTML5World/src/resource.js
@@ -0,0 +1,29 @@
+var s_HelloWorld = "res/HelloWorld.png";
+var s_CloseNormal = "res/CloseNormal.png";
+var s_CloseSelected = "res/CloseSelected.png";
+
+var g_resources = [
+    //image
+    {src:s_HelloWorld},
+    {src:s_CloseNormal},
+    {src:s_CloseSelected}
+
+    //plist
+
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
+
+var s_menuItem = "res/Images/menuitemsprite.png";
+var s_sendScore = "res/Images/SendScoreButton.png";
+var s_pressSendScore = "res/Images/SendScoreButtonPressed.png";
+var s_fpsImages = "res/fonts/fps_images.png";
+var s_normalShare = "res/Images/normal_share.png";
+var s_selectShare = "res/Images/select_share.png";
+
+var s_bitmapFontTest3_fnt = "res/fonts/bitmapFontTest3.fnt";
